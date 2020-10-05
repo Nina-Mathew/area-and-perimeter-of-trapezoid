db353e757f4d1d334aacefcac488d23ccb573bb1
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-05 14:37:05 -0400

    sides of trapezoid

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
 // Created by: Nina Mathew
 // Created on: 10/5/20
 // This program finds the area and perimeter of a trapezoid
+let area = 0
+game.splash("Lets calculate the area and perimeter of a trapezoid!")
+let A_Base = game.askForNumber("Enter the A Base of the trapezoid")
+let B_Base = game.askForNumber("Enter the B Base of the trapezoid")
+let C_Base = game.askForNumber("Enter the C Base of the trapezoid")
+let D_Base = game.askForNumber("Enter the D Base of the trapezoid")
+let perimeter = A_Base + (B_Base + (C_Base + D_Base))
+game.splash(convertToText(perimeter))
+let height = game.askForNumber("Enter the Height of the trapezoid")
+A_Base = game.askForNumber("Enter the A Base of the trapezoid")
+B_Base = game.askForNumber("Enter the B Base of the trapezoid")
+height = area + area / (A_Base + B_Base)
+area = height * (A_Base + B_Base / 2)
+game.splash(convertToText(area))
+game.splash("Done")
